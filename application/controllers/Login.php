29e6cb17fb87d10fc3df01998aea0c808e13f508
Muhammad_Abduh_Husaini_Batubara <abduhbatubara@student.telkomuniversity.ac.id>
2021-06-02 20:49:35 +0700

    Login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,10 +1,31 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Login extends CI_Controller
+{
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper(array('form', 'url'));
+		$this->load->library('form_validation');
+		$this->load->model('M_Login');
+	}
 	public function index()
 	{
 		$this->load->view('login');
+	}
+	public function Auth_Login()
+	{
+		$username = $this->input->POST('username');
+		$password = $this->input->POST('password');
+		$cekUser = $this->M_Login->Login($username, $password, 'akun_pengguna');
+		if ($cekUser) {
+			$this->session->set_userdata('status', 'login');
+			redirect('Home');
+		} else {
+			$data['error'] = 'Username dan Password Salah!!';
+			$this->load->view('login', $data);
+		}
 	}
 }
---
+++ application/models/M_Login.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class M_Login extends CI_Model
+{
+    public function Login($username, $password, $table)
+    {
+        $this->db->select('*');
+        $this->db->from($table);
+        $this->db->where('username', $username);
+        $this->db->where('password', $password);
+        $this->db->limit(1);
+
+        $query = $this->db->get();
+        if ($query->num_rows() == 1) {
+            return $query->result();
+        } else {
+            return false;
+        }
+    }
+}
